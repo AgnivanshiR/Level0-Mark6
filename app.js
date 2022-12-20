71ac8055baf8eda82c7eeef690e63dc8a2c36c9b
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2022-12-20 18:32:09 +0530

    done

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 var btnTranslate = document.querySelector(".btn-translate");
 
 var txtInput = document.querySelector("#textArea");
+var output = document.querySelector('#output');
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!")
-    console.log("input", txtInput.value)
-})
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslateURL(text) {
+    return serverURL + "?" + "text=" +text
+}
+console.log(getTranslateURL)
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslateURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText; // output
+           })
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
+// ********************************
 
 
 
+
+// for tags like textarea query will be- document.querySelector("textarea")
+// for class- document.querySelector(".btn-primary")
+// for id- document.querySelector("#input-btn")
+// for input element with an attribute name='translator'- document.querySelector("input[name='translator']")
