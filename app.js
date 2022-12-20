4b66e82aacfb6c5ba50944ed88786418b339cfef
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2022-12-20 17:46:42 +0530

    Taking-input-console

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("script is working")
+var btnTranslate = document.querySelector(".btn-translate");
+
+var txtInput = document.querySelector("#textArea");
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!")
+    console.log("input", txtInput.value)
+})
+
+
+
